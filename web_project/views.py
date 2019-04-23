a13552f0f092a7cf45692e16dc43fa5a45fb69db
Ivan Podorozhnyi <ivan.podorozhnyi@gmail.com>
2019-04-23 17:43:10 +0300

    add blog app

--- web_project/views.py
+++ web_project/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
@@ -15,3 +16,10 @@ def about_page(request):
 
 def contact_page(request):
     return render(request, "hello_world.html", {"title": "Contact us"})
+
+
+def example_page(request):
+    context = {"title": "Example"}
+    template_name = "hello_world.html"
+    template_obj = get_template(template_name)
+    return HttpResponse(template_obj.render(context))
