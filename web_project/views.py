6383ab18846898eb4c8a57ddbca6365292d990fa
Ivan Podorozhnyi <ivan.podorozhnyi@gmail.com>
2019-05-13 15:17:26 +0300

    Add contact form

--- web_project/views.py
+++ web_project/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
 
+from .forms import ContactForm
 
 def home_page(request):
     my_title = "Hello there ...."
@@ -16,7 +17,12 @@ def about_page(request):
 
 
 def contact_page(request):
-    return render(request, "hello_world.html", {"title": "Contact us"})
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form = ContactForm()
+    context = {"title": "Contact us", "form": form}
+    return render(request, "form.html", context)
 
 
 def example_page(request):
