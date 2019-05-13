5121bbd3d472d14dca369fa1e4aac6ba7d4d081e
Ivan Podorozhnyi <ivan.podorozhnyi@gmail.com>
2019-05-13 14:47:54 +0300

    Add views and templates for crud operations with Blog posts

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,35 @@ from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
 
-def blog_post_detail_page(request, slug):
-    queryset = BlogPost.objects.filter(slug=slug)
-    if queryset.count() == 0:
-        raise Http404
-    obj = queryset.first()
-    # obj = get_object_or_404(BlogPost, slug=slug)
+def blog_post_create_view(request):
+    template_name = 'blog_post_create.html'
+    context = {'form': None}
+    return render(request, template_name, context)
+
+
+def blog_post_list_view(request):
+    qs = BlogPost.objects.all()
+    template_name = 'blog_post_list.html'
+    context = {'object_list': qs}
+    return render(request, template_name, context)
+
+
+def blog_post_detail_view(request,slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
+    context = {"object": obj}
+    return render(request, template_name, context)
+
+
+def blog_post_update_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_update.html'
+    context = {"object": obj, 'form': None}
+    return render(request, template_name, context)
+
+
+def blog_post_delete_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_delete.html'
     context = {"object": obj}
     return render(request, template_name, context)
