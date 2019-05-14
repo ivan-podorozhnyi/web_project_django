b442554e6b22e6d71e630033515fbd18c7e095cf
Ivan Podorozhnyi <ivan.podorozhnyi@gmail.com>
2019-05-14 13:35:03 +0300

    Modify update and delete views

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,20 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from .forms import BlogPostModelForm
 from .models import BlogPost
 
 
+# @login_required
+@staff_member_required
 def blog_post_create_view(request):
     form = BlogPostModelForm(request.POST or None)
     if form.is_valid():
         obj = form.save(commit=False)
+        obj.user = request.user
         obj.save()
         form = BlogPostModelForm()
     template_name = 'form.html'
@@ -31,15 +36,23 @@ def blog_post_detail_view(request,slug):
     return render(request, template_name, context)
 
 
+@staff_member_required
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog/update.html'
-    context = {"object": obj, 'form': None}
+    form = BlogPostModelForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    template_name = 'form.html'
+    context = {"form": form, 'title': f"Update {obj.title}"}
     return render(request, template_name, context)
 
 
+@staff_member_required
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog/delete.html'
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('/blog')
     context = {"object": obj}
     return render(request, template_name, context)
