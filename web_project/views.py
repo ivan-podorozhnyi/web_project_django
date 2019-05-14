a66a75638afa355d0354a02e064ad811f7f9f356
Ivan Podorozhnyi <ivan.podorozhnyi@gmail.com>
2019-05-14 14:56:49 +0300

    Add navbar

--- web_project/views.py
+++ web_project/views.py
@@ -3,13 +3,12 @@ from django.shortcuts import render
 from django.template.loader import get_template
 
 from .forms import ContactForm
+from blog.models import BlogPost
 
 
 def home_page(request):
-    my_title = "Hello there ...."
-    context = {"title": "my title"}
-    if request.user.is_authenticated:
-        context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
+    qs = BlogPost.objects.all()[:5]
+    context = {"title": "Welcome to my blog", "blog_list": qs}
     return render(request, "home.html", context)
 
 
